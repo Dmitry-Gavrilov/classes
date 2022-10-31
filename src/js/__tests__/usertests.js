06fa9c90bb9cd1b27e778a03cfdf574039e530f8
Dmitry-Gavrilov <91532052+Dmitry-Gavrilov@users.noreply.github.com>
2022-10-31 22:28:26 +1000

    1

---
+++ src/js/__tests__/usertests.js
@@ -0,0 +1,52 @@
+import Characters from '../characters';
+import Bowman from '../bowman';
+import Daemon from '../daemon';
+import Magician from '../magician';
+import Swordsman from '../swordsman';
+import Undead from '../undead';
+import Zombie from '../zombie';
+
+test('longName', () => {
+  const named = () => new Characters('df1227777777777777', 'Bowman');
+  expect(named).toThrowError('Длина имени должна быть от 2 до 10 символов');
+});
+
+test('shortName', () => {
+  const named = () => new Characters('ы', 'Zombie', 100);
+  expect(named).toThrowError('Длина имени должна быть от 2 до 10 символов');
+});
+
+test('typeError', () => {
+  const named = () => new Characters('ironMan', 'Axeman');
+  expect(named).toThrowError('Неверный тип.');
+});
+
+test('Zombie parameters', () => {
+  const zombie = new Zombie('Zooma');
+  expect([zombie.type, zombie.health, zombie.attack, zombie.defence]).toEqual(['Zombie', 100, 40, 10]);
+});
+
+test('Bowman parameters', () => {
+  const bowman = new Bowman('RobinGood');
+  expect([bowman.type, bowman.health, bowman.attack, bowman.defence]).toEqual(['Bowman', 100, 25, 25]);
+});
+
+test('Undead parameters', () => {
+  const undead = new Undead('Koschei');
+  expect([undead.type, undead.level, undead.attack, undead.defence]).toEqual(['Undead', 1, 25, 25]);
+});
+
+test('Magician parameters', () => {
+  const magician = new Magician('Merlin');
+  expect([magician.type, magician.level, magician.attack, magician.defence]).toEqual(['Magician', 1, 10, 40]);
+});
+
+test('Daemon parameters', () => {
+  const daemon = new Daemon('Гагтунгр ');
+  expect([daemon.type, daemon.health, daemon.attack, daemon.defence]).toEqual(['Daemon', 100, 10, 40]);
+});
+
+test('Swordsman parameters', () => {
+  const swordsman = new Swordsman('Артур');
+  expect([swordsman.type, swordsman.health, swordsman.attack, swordsman.defence]).toEqual(['Swordsman', 100, 40, 10]);
+});
